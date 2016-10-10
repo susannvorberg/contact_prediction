2a0bd4b0df9b9430ecc259f8da3ed0f129384e1b
Susann Vorberg <susann.vorberg@gmail.com>
2016-10-10 14:39:06 +0200

    plots of some alignment stats

---
+++ plotting/plot_alignment_aminoacid_distribution.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# 	This scripts plots the distribution of amino acids per position in the alignment
+#
+###############################################################################
+
+#===============================================================================
+#== libraries
+#===============================================================================
+import argparse
+import os
+import numpy as np
+from collections import Counter
+import plotly.graph_objs as go
+from plotly.offline import plot as plotly_plot
+import utils.io_utils as io
+
+
+
+def plot_amino_acid_distribution_per_position(alignment_file, plot_file):
+
+    # read alignment
+    protein = os.path.basename(alignment_file).split(".")[0]
+    alignment = io.read_alignment(alignment_file)
+    N = float(len(alignment))
+    L = len(alignment[0])
+
+    # compute percentage of gaps per position
+    alignment = alignment.transpose()
+
+
+    #create plot
+    data = []
+
+    aa_freq_per_pos = np.zeros((21, L))
+    for position in range(L):
+        aa_counts = Counter(alignment[position])
+        for aa, counts in aa_counts.iteritems():
+            freq = counts/N
+            aa_freq_per_pos[aa,position] = freq
+
+    #add bar for each amino acid for each position
+    for aa in range(1,21):
+        data.append(go.Bar(
+                      x=range(L),
+                      y=aa_freq_per_pos[aa],
+                      showlegend=True,
+                      name=AMINO_ACIDS[aa]
+              ))
+
+
+    layout = go.Layout(
+        barmode='stack',
+        title="Distribution of Amino Acids per position in alignment of " + str(protein) + "<br> N="+str(N) + ", L="+str(L),
+        xaxis=dict(title="Alignment Position"),
+        yaxis=dict(title="Amino Acid Distribution"),
+        font=dict(size=18)
+    )
+
+    plot = {'data': data, 'layout': layout}
+    plotly_plot(plot, filename=plot_file, auto_open=False)
+
+
+def main():
+
+    ### Parse arguments
+    parser = argparse.ArgumentParser(description='Plotting a contact map.')
+    parser.add_argument("alignment_file",       type=str,   help="path to aligment file")
+    parser.add_argument("plot_file",            type=str,   help="path to plot file")
+
+    args = parser.parse_args()
+
+    alignment_file              = str(args.alignment_file)
+    plot_file                   = str(args.plot_file)
+    #alignment_file = "/home/vorberg/work/data/benchmarkset_cathV4/benchmarkset_cathV4_combs/psc_eval01/3cb2_A_03.psc"
+    #plot_file = "/home/vorberg/test.html"
+
+    plot_amino_acid_distribution_per_position(alignment_file, plot_file)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,5 +1,37 @@
 import os
 import json
+import numpy as np
+
+AMINO_INDICES = {
+     'A' : 1,
+     'B' : 0,
+     'C' : 5,
+     'D' : 4,
+     'E' : 7,
+     'F' : 14,
+     'G' : 8,
+     'H' : 9,
+     'I' : 10,
+     'J' : 0,
+     'K' : 12,
+     'L' : 11,
+     'M' : 13,
+     'N' : 3,
+     'O' : 0,
+     'P' : 15,
+     'Q' : 6,
+     'R' : 2,
+     'S' : 16,
+     'T' : 17,
+     'U' : 0,
+     'V' : 20,
+     'W' : 18,
+     'X' : 0,
+     'Y' : 19,
+     'Z' : 0,
+     '-' : 0}
+
+AMINO_ACIDS = "-ARNDCQEGHILKMFPSTWYV"
 
 def read_json_from_mat(matfile):
     '''
@@ -22,3 +54,23 @@ def read_json_from_mat(matfile):
                 print(str(matfile) + "does not contain META info. (Line must start with #META!)")
 
     return meta
+
+def read_alignment(alignment_file):
+    """
+    Read alignment file (Psicov Format)
+
+    :param alignment_file: path to alignment file
+    :return: matrix of alingnment
+    """
+    alignment = None
+
+    try:
+        f = open(alignment_file)
+        alignment = np.array([[AMINO_INDICES[c] for c in x.strip()] for x in f], dtype=np.uint8)
+
+        f.close()
+    except IOError:
+        print ("Could not open psicov file: " + alignment_file )
+
+
+    return alignment
