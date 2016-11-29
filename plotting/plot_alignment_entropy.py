af0cd833d66e56028ae6d5b118dff05ffb855487
Susann Vorberg <susann.vorberg@gmail.com>
2016-11-29 16:35:31 +0100

    initial commit plot alignment entropy (base 2)

--- plotting/plot_alignment_entropy.py
+++ plotting/plot_alignment_entropy.py
@@ -13,9 +13,10 @@ from collections import Counter
 import plotly.graph_objs as go
 from plotly.offline import plot as plotly_plot
 import utils.io_utils as io
+from scipy.stats import entropy
 
 
-def plot_percentage_gaps_per_position(alignment_file, plot_dir=None):
+def plot_alignment_entropy(alignment_file, plot_dir=None):
 
     # read alignment
     protein = os.path.basename(alignment_file).split(".")[0]
@@ -23,25 +24,38 @@ def plot_percentage_gaps_per_position(alignment_file, plot_dir=None):
     N = float(len(alignment))
     L = len(alignment[0])
 
-    # compute percentage of gaps per position
+
     alignment = alignment.transpose()
-    gaps = [Counter(alignment[pos])[0] / N for pos in range(L)]
+
+    #determine amino acid frequencies (without any pseudocounts)
+    aa_freq_per_pos = np.zeros((21, L))
+    for position in range(L):
+        aa_counts = Counter(alignment[position])
+        for aa, counts in aa_counts.iteritems():
+            freq = counts/N
+            aa_freq_per_pos[aa,position] = freq
+
+    aa_freq_per_pos = aa_freq_per_pos[1:]#remove gaps
+    aa_freq_per_pos = aa_freq_per_pos.transpose()
+
+    entropy_per_position = [entropy(aa_freq_per_pos[pos], base=2) for pos in range(L)]
+
 
     #create plot
     data = []
     data.append(
         go.Scatter(
             x=[x for x in range(L)],
-            y=gaps,
+            y=entropy_per_position,
             name = "percentage of gaps",
             mode="Lines"
         )
     )
 
     layout = {
-        'title':"Percentage of gaps in alignment of " + str(protein) + "<br> N="+str(N) + ", L="+str(L),
+        'title':"Entropy (base 2) in alignment of " + str(protein) + "<br> N="+str(N) + ", L="+str(L),
         'xaxis':{'title':"Alignment Position"},
-        'yaxis':{'title':"Percentage of Gaps"},
+        'yaxis':{'title':"Entropy "},
         'font':{'size':18}
     }
 
@@ -49,16 +63,15 @@ def plot_percentage_gaps_per_position(alignment_file, plot_dir=None):
     if plot_dir is None:
         return plot
     else:
-        plot_file = plot_dir +"/alignment_percentage_gaps_" + protein + ".html"
+        plot_file = plot_dir +"/alignment_entropy_" + protein + ".html"
         plotly_plot(plot, filename=plot_file, auto_open=False)
-
 
 
 
 def main():
 
     ### Parse arguments
-    parser = argparse.ArgumentParser(description='Plotting coverage of a alignment.')
+    parser = argparse.ArgumentParser(description='Plotting entropy of a alignment.')
     parser.add_argument("alignment_file",       type=str,   help="path to aligment file")
     parser.add_argument("plot_dir",            type=str,   help="path to plot directory")
 
@@ -69,7 +82,8 @@ def main():
     #alignment_file = "/home/vorberg/work/data/benchmarkset_cathV4/benchmarkset_cathV4_combs/psc_eval01/1h4x_A_00.psc"
     #plot_dir = "/home/vorberg/"
 
-    plot_percentage_gaps_per_position(alignment_file, plot_dir)
+    plot_alignment_entropy(alignment_file, plot_dir)
+
 
 if __name__ == '__main__':
     main()
