38c14b4fe8c1a440998396c7f8333ee7ea28c4ed
Susann Vorberg <susann.vorberg@gmail.com>
2017-02-03 13:47:06 +0100

    splitted function into create eval file and annotate eval file

--- benchmark/create_evaluation_file.py
+++ benchmark/create_evaluation_file.py
@@ -10,19 +10,11 @@ import pandas as pd
 import utils.pdb_utils as pdb
 
 
-def create_evaluation_file(pdb_file, eval_dir, seqsep, N, L, neff, cath, name):
+def create_evaluation_file(evaluation_file, pdb_file, seqsep):
 
-    #===============================================================================
-    ### Read PDb and get residue pairs (i,j) with (j-i) > seqsep
-    #===============================================================================
 
     #determine indices that are resolved in PDB and have minimal required seq sep
     distance_matrix = pdb.distance_map(pdb_file)
-
-    #protein length
-    #might be smaller than real L if C-terminal residues are not resolved in PDB
-    if L == 0:
-        L=len(distance_matrix)
 
     #get residue pairs that are resolved and (j-i) > seqsep
     indices_pairs_resolved  = zip(*np.where(~np.isnan(distance_matrix)))
@@ -30,10 +22,7 @@ def create_evaluation_file(pdb_file, eval_dir, seqsep, N, L, neff, cath, name):
     ij_indices = list(set(indices_pairs_resolved).intersection(indices_pairs_seqsep))
 
 
-    #===============================================================================
-    ### Create the evaluation file
-    #===============================================================================
-
+    #Create the evaluation file
     eval_df = pd.DataFrame(
         {
             'i': list(zip(*ij_indices)[0]),
@@ -45,11 +34,13 @@ def create_evaluation_file(pdb_file, eval_dir, seqsep, N, L, neff, cath, name):
 
 
     #write evaluation dataframe to file
-    eval_file_name = eval_dir + "/" + name + ".eval"
-    eval_df.to_csv(eval_file_name, sep="\t", header=True, index=False)
+    eval_df.to_csv(evaluation_file, sep="\t", header=True, index=False)
+
+
+def annotate_evalutation_file(evaluation_file, N, L, neff, cath, name):
 
     #write meta_data to file
-    eval_metafile_name = eval_dir + "/" + name + ".meta"
+    eval_metafile_name = evaluation_file.replace("eval", "meta")
     with open(eval_metafile_name, 'w') as fp:
         json.dump({'protein' : {"name": name,
                                 "N": N,
@@ -60,26 +51,24 @@ def create_evaluation_file(pdb_file, eval_dir, seqsep, N, L, neff, cath, name):
                                 }
                    }, fp)
 
+
 def main():
 
     # ===============================================================================
     ### Parse arguments
     # ===============================================================================
 
-    parser = argparse.ArgumentParser(
-    description='Create an evaluation file per protein: table with columns: i,j,Cbeta,L,N,neff,cath')
+    parser = argparse.ArgumentParser(description='Create an evaluation file per protein: table with columns: i,j,Cbeta,L,N,neff,cath')
 
     parser.add_argument("pdb_file", type=str, help="path to pdb file")
     parser.add_argument("eval_dir", type=str, help="directory for evaluation file")
     parser.add_argument("--seqsep", type=int, default=6, help="sequence separation")
-    parser.add_argument("--contact_threshold", type=int, default=8,
-                        help="contact definition; C_beta distance between residue pairs")
+    parser.add_argument("--contact_threshold", type=int, default=8, help="contact definition; C_beta distance between residue pairs")
     parser.add_argument("--N", type=int, default=0, help="number of sequences in alignment")
     parser.add_argument("--L", type=int, default=0, help="protein length (otherwise L will be determined from pdb file)")
     parser.add_argument("--neff", type=int, default=0, help="effective number of sequences in alignment")
     parser.add_argument("--cath", type=int, default=0, help="cath class")
-    parser.add_argument("--name", type=str,
-                        help="protein name = eval file name (otherwise name is parsed from pdb file)")
+    parser.add_argument("--name", type=str, help="protein name = eval file name (otherwise name is parsed from pdb file)")
 
     args = parser.parse_args()
 
@@ -87,7 +76,11 @@ def main():
     if name is None:
         name = os.path.basename(args.pdb_file).split(".")[0]
 
-    create_evaluation_file(args.pdb_file, args.eval_dir, args.seqsep, args.N, args.L, args.neff, args.cath, name)
+    evaluation_file =  args.eval_dir + "/" + name + ".eval"
+
+
+    create_evaluation_file(evaluation_file, args.pdb_file, args.seqsep)
+    annotate_evalutation_file(evaluation_file, args.N, args.L, args.neff, args.cath, name)
 
 
 
