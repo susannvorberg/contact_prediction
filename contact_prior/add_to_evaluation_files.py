0a21dda7ec1644993cc217fd54ba5a57ad47446a
Susann Vorberg <susann.vorberg@gmail.com>
2017-07-28 17:27:36 +0200

    added functionality: add to eval suite

--- contact_prior/add_to_evaluation_files.py
+++ contact_prior/add_to_evaluation_files.py
@@ -12,20 +12,19 @@ def parse_args():
     input = parser.add_argument_group("Input Files for Feature Generation")
     input.add_argument("property_files_dir",   default=None, help="Path to dataset fold property files")
     input.add_argument("alignment_dir",    type=str, help="path to alignment files")
-    input.add_argument("pdb_dir",          type=str, help="path to pdb files")
     input.add_argument("psipred_dir",      type=str, help="path to psipred predictions")
     input.add_argument("netsurfp_dir",     type=str, help="path to netsurfp predictions")
     input.add_argument("mi_dir",           type=str, help="path to MI coevolution scores")
     input.add_argument("omes_dir",         type=str, help="path to OMES coevolution scores")
-    input.add_argument("--braw_dir",       type=str, default=None,     help="path to braw files")
     input.add_argument("model_file",             type=str, help="path to random forest model")
 
     dataset = parser.add_argument_group("Settings for Feature Generation")
     dataset.add_argument("--n_proteins",     type=int, default=200,     help="size of benchmark set")
 
     out = parser.add_argument_group("Output directory for benchmark plots")
-    out.add_argument("plot_dir",          type=str, help="Path for plots")
-    out.add_argument("evaluation_dir",    type=str, help="Path to evaluation files")
+    out.add_argument("plot_dir",            type=str, help="Path for plots")
+    out.add_argument("evaluation_dir",      type=str, help="Path to evaluation files")
+    out.add_argument("name",                type=str, help="name of method in evaluation suite")
 
     args = parser.parse_args()
 
@@ -38,19 +37,14 @@ def main():
 
     property_files_dir      = args.property_files_dir
     alignment_dir           = args.alignment_dir
-    pdb_dir                 = args.pdb_dir
     psipred_dir             = args.psipred_dir
     netsurfp_dir            = args.netsurfp_dir
     mi_dir                  = args.mi_dir
     omes_dir                = args.omes_dir
-    braw_dir                = args.braw_dir
     model_file              = args.model_file
     n_proteins              = args.n_proteins
-    plot_dir                = args.plot_dir
     evaluation_dir          = args.evaluation_dir
-
-
-
+    method_name = args.name
 
 
     ###########  Setup dataset_id
@@ -64,62 +58,33 @@ def main():
     ########## Setup Benchmark framework
     b = Benchmark(evaluation_dir)
 
-    ########## Benschmark on dataset 10
-    benchmark_dataset_id = 10
+    ########## Benchmark on these datasets
+    benchmark_dataset_id = [6,7,8]
 
     ######### Load model
     rf_clf, rf_meta = BayesianContactPredictor.load_contact_prior_model(model_file)
 
     ########## Iterate over proteins
-    for protein in dataset_properties.query('dataset_id == '+str(benchmark_dataset_id))['protein'][:n_proteins]:
+    for protein in dataset_properties.query('dataset_id in @benchmark_dataset_id')['protein'][:n_proteins]:
 
         alignment_file  = alignment_dir + "/" + protein.strip() + ".filt.psc"
         psipred_file    = psipred_dir + "/" + protein.strip() + ".filt.withss.a3m.ss2"
         netsurfp_file   = netsurfp_dir + "/" + protein.strip() + ".filt.netsurfp"
-        mi_file         = mi_dir + "/"+ protein.strip() + "filt.mi..mat"
-        omes_file       = omes_dir + "/"+ protein.strip() + "filt.omes.mat"
+        mi_file = mi_dir + "/" + protein.strip() + ".filt.mi.pc.mat"
+        omes_file = omes_dir + "/" + protein.strip() + "filt.omes.fodoraldrich.mat"
 
-        BCP = BayesianContactPredictor(alignment_file, rf_meta['seq_separation'], rf_meta['contact_threshold'])
+        BCP = BayesianContactPredictor(alignment_file, rf_meta['training_set']['sequence_separation'], rf_meta['training_set']['contact_threshold'])
         BCP.contact_prior_model = rf_clf
         BCP.contact_prior_meta = rf_meta
-        contact_prior_mat = BCP.contact_prior(psipred_file, netsurfp_file, mi_file, omes_file)
+        BCP.contact_prior(psipred_file, netsurfp_file, mi_file, omes_file)
+        contact_prior_mat = BCP.get_contact_prior(contact=1)
         meta = {
             'opt_code' : 1,
             'rf' : rf_meta
         }
 
-        b.add_method(protein, 'rf+apc', contact_prior_mat, meta, apc=True, update=True)
-        b.add_method(protein, 'rf', contact_prior_mat, meta, apc=False, update=True)
-
-    ########## Plotting
-
-    #Filter options
-    filter_optcode_0 = {'key':'opt_code', 'value':0, 'operator':'greater_equal'}
-    b.add_filter(filter_optcode_0)
-    print(b.filter)
-
-    #Set methods to compare
-    benchmark_methods = ['ccmpred-pll-centerv+apc']
-    benchmark_methods += ['rf', 'rf+apc', 'omes+apc', 'mi+apc']
-
-    #Compute statistics ===================================================================================================
-    seqsep = rf_meta['seq_separation']
-    contact_thr = rf_meta['contact_threshold']
-    b.set_methods_for_benchmark(benchmark_methods)
-    b.compute_evaluation_statistics(seqsep, contact_thr)
-
-    #Plot =================================================================================================================
-    plot_types=[]
-    plot_types.append('precision_vs_rank')
-    plot_types.append('precision_per_protein')
-    plot_types.append('meanerror_rank')
-    plot_types.append('facetted_by_div')
-    plot_types.append('facetted_by_neff')
-    plot_types.append('facetted_by_cath')
-    plot_types.append('facetted_by_L')
-    plot_types.append('meanprecision_by_neff')
-    plot_types.append('meanprecision_by_div')
-    b.plot(plot_dir, plot_type=plot_types)
+        b.add_method(protein, method_name + '+apc', contact_prior_mat, meta, apc=True, update=True)
+        b.add_method(protein, method_name, contact_prior_mat, meta, apc=False, update=True)
 
 
 
