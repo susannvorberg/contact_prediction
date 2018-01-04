d4a9250a6a15aec25886841d52b3c8f0d36fcda8
Susann Vorberg <susann.vorberg@gmail.com>
2018-01-04 14:20:51 +0100

    ccmraw is now in utils

--- utils/benchmark_utils.py
+++ utils/benchmark_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-import raw
+import ccmraw as raw
 import json
 
 def subset_evaluation_dict(evaluation_statistics, bins, subset_property, methods, evaluation_measure):
