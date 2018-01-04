33139e6267d22b4b9d3d1c03cf950883a2cb8f81
Susann Vorberg <susann.vorberg@gmail.com>
2018-01-04 10:15:59 +0100

    added find_dcit_key funciton

--- utils/utils.py
+++ utils/utils.py
@@ -11,3 +11,19 @@ def gen_dict_extract(key, var):
                 for d in v:
                     for result in gen_dict_extract(key, d):
                         yield result
+
+def find_dict_key(key, dictionary):
+    for k, v in dictionary.items():
+        if k == key:
+            return v
+        if isinstance(v, dict):
+            res = find_dict_key(key, v)
+            if res != 0:
+                return res
+        if isinstance(v, list):
+            for d in v:
+                res = find_dict_key(key, d)
+                if res != 0:
+                    return res
+
+    return 0
